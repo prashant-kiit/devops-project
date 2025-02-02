10f0f4618b20ce34a06bce7ecc834c0bfb6d0007
prashant-nsws <prashant.singh@neurosparkworks.com>
2025-02-02 15:49:17 +0530

    infra: added bundler

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,1 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import router from "./routes/routes";
-
-export const app = express();
-export const port = 3000;
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use("/app", router);
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+export * as app from "./src/server";
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import router from "./routes/routes";
+
+export const app = express();
+export const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/app", router);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+export default app;
