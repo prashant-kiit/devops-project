053ca0da3bc076a61fcf054fca989bbce38f9e16
prashant-nsws <prashantsingh090798@gmail.com>
2025-02-09 21:30:06 +0530

    feat: added 4 instances of backend server

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import router from "./routes/routes";
 
 export const app = express();
-export const port = 3000;
+export const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
