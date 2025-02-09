a9abe916b9ed23176af72cfba0944004fca6c4b6
prashant-nsws <prashantsingh090798@gmail.com>
2025-02-10 03:11:22 +0530

    chore: added env variables

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -2,7 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import router from "./routes/routes";
+import dotenv from "dotenv";
 
+const environment = process.env.ENV;
+dotenv.config({ path: `../../.env.${environment}` });
 export const app = express();
 export const port = process.env.PORT;
 
@@ -12,7 +15,7 @@ app.use(bodyParser.json());
 app.use("/app", router);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server started on port http://localhost:${port} in ${environment} mode`);
 });
 
 export default app;
