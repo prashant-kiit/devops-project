4355bde64a731436e873712453113b120efc7ac6
prashant-nsws <prashantsingh090798@gmail.com>
2025-02-16 02:28:56 +0530

    chore: added env prod fro backend to adjust db url in env

--- app/backend/config.ts
+++ app/backend/config.ts
@@ -1,9 +1,8 @@
 import path from "path";
 import dotenv from "dotenv";
 
-if (process.env.ENV === "development") {
-  dotenv.config({ path: path.resolve(".env.development") });
-}
+const env = process.env.ENV;
+dotenv.config({ path: path.resolve(`.env.${env}`) });
 
 const config = {
   BACKEND_PORT: process.env.BACKEND_PORT,
